a38f545a73b1321ea1b91723621fc3cc51370339
Dirk van Oosterbosch <dirk@innovactory.com>
2015-07-10 15:52:25 +0200

    Initial Commit

---
+++ ActionKit/ActionKit/ActionKit.h
@@ -0,0 +1,19 @@
+//
+//  ActionKit.h
+//  ActionKit
+//
+//  Created by Dirk van Oosterbosch on 10/07/15.
+//  Copyright (c) 2015 Dirk van Oosterbosch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ActionKit.
+FOUNDATION_EXPORT double ActionKitVersionNumber;
+
+//! Project version string for ActionKit.
+FOUNDATION_EXPORT const unsigned char ActionKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ActionKit/PublicHeader.h>
+
+
