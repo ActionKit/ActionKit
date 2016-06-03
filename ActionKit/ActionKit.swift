d55aa4a81898e4db7e21f60c0af0c249d435c8a1
Dirk van Oosterbosch <dirk@innovactory.com>
2016-06-03 21:36:02 +0200

    Add more documentation and explanation of the bugs and workaround with closures and closure parameters

--- ActionKit/ActionKit.swift
+++ ActionKit/ActionKit.swift
@@ -9,6 +9,79 @@
 import Foundation
 import UIKit
 
+//: ## Having an UIControl as optional input parameter for the closure
+//:
+//: We like to have the UIControl as input parameter in the closure, like this:
+//:
+//: ```swift
+//:     button.addControlEvent(.TouchUpInside) { (control: UIControl) in
+//:         print("the control: \(control) was tapped")
+//:     }
+//: ```
+//:
+//: But unfortunately, there is a bug in the swift compiler, that prevents implicit
+//: arguments in the closure. This bug will cause an error when the argument is ignored
+//: implicitly. That means that if you would try to use a closure which has a argument
+//: `(control: UIContol)`, without that argument like this:
+//:
+//: ```swift
+//:     button.addControlEvent(.TouchUpInside) {
+//:         print("this control was tapped")
+//:     }
+//: ```
+//:
+//: Then the compiler will give an error:
+//:
+//: ```Contextual type for closure argument list expects 1 argument, which cannot be implicitlty ignored```
+//:
+//: See Chris Lattner's option on the matter (that this is a compiler bug):
+//: [http://article.gmane.org/gmane.comp.lang.swift.evolution/17196/](http://article.gmane.org/gmane.comp.lang.swift.evolution/17196/)
+//:
+//: ### Workaround by having multiple functions
+//:
+//: We can workaround this bug by having multiple functions, one without the `UIControl` parameter
+//: (the original) and one with the `(control: UIControl) in` parameter. The only downside is that
+//: you will have to call the closure with a specific signature, so either with:
+//:
+//: ```swift
+//:     button.addControlEvent(.TouchUpInside) {
+//:         ...
+//:     }
+//: ```
+//:
+//: or with:
+//:
+//: ```swift
+//:     button.addControlEvent(.TouchUpInside) { (control: UIControl) in
+//:         ...
+//:     }
+//: ```
+//:
+//: but not with:
+//:
+//: ```swift
+//:     button.addControlEvent(.TouchUpInside) { aButton in
+//:         ...
+//:     }
+//: ```
+//:
+//: or with
+//:
+//: ```swift
+//:     button.addControlEvent(.TouchUpInside) { _ in
+//:         ...
+//:     }
+//: ```
+//:
+//: Because `aButton` or `_` is not specific enough and the compiler will complain with:
+//:
+//: ```Ambiguous use of 'addControlEvent'```
+//:
+//: So, until this bug is solved in the Swift compiler, we will have to use **two** functions
+//: for each control event, and be specific with the arguments in the closure call.
+//:
+
+
 typealias ActionKitVoidClosure = () -> Void
 typealias ActionKitControlClosure = (UIControl) -> Void
 typealias ActionKitGestureClosure = (UIGestureRecognizer) -> Void
@@ -30,14 +103,14 @@ class ActionKitSingleton {
         return ActionKit.instance
     }
 }
-//
-//  GESTURE ACTIONS
-//
+
+//:
+//: # GESTURE ACTIONS
+//:
     
 extension ActionKitSingleton {
     
     func addGestureClosure(gesture: UIGestureRecognizer, name: String, closure: ActionKitClosure) {
-//        gestureDict[gesture] = closure
         if var gestureArr = gestureDict[gesture] {
             gestureArr.append(name, closure)
             gestureDict[gesture] = gestureArr
@@ -62,7 +135,6 @@ extension ActionKitSingleton {
     }
     
     func removeGesture(gesture: UIGestureRecognizer, name: String) {
-//        gestureDict.removeValueForKey(gesture)
         if var gestureArray = gestureDict[gesture] {
             var x: Int = 0
             for (index, gestureTuple) in gestureArray.enumerate() {
@@ -99,9 +171,10 @@ extension ActionKitSingleton {
     
 }
 
-//
-// CLOSURE ACTIONS
-//
+//:
+//: # CLOSURE ACTIONS
+//:
+
 extension ActionKitSingleton {
     func removeAction(control: UIControl, controlEvent: UIControlEvents) {
         if var innerDict = controlAndEventsDict[control] {
@@ -121,6 +194,7 @@ extension ActionKitSingleton {
             controlAndEventsDict[control] = newDict
         }
     }
+    
     
     // Start the 19 different runClosure methods, each responding to a different UIControlEvents
     @objc(runClosureTouchDown:)
