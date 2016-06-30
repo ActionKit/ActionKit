47dff3559d72bfe51d89fccf9078627a05677971
Manuel G-Estañ <manuel.garcia.estan@gmail.com>
2016-06-30 18:03:06 +0200

    Add documentation

--- ActionKit/UIBarButtonItem+ActionKit.swift
+++ ActionKit/UIBarButtonItem+ActionKit.swift
@@ -13,8 +13,20 @@ private extension Selector {
 	static let runBarButtonItem = #selector(ActionKitSingleton.runBarButtonItem(_:))
 }
 
+/** 
+ Extension to provide convenience initializers to use closures instead of target/actions.
+*/
 public extension UIBarButtonItem {
 	
+	/** 
+	Initializes a new item using the specified image and other properties.
+	
+	- parameter image: The images displayed on the bar are derived from this image. If this image is too large to fit on the bar, it is scaled to fit. Typically, the size of a toolbar and navigation bar image is 20 x 20 points. The alpha values in the source image are used to create the images—opaque values are ignored.
+	- parameter landscapeImagePhone: The style of the item. One of the constants defined in UIBarButtonItemStyle. nil by default
+	- parameter style: The style of the item. One of the constants defined in UIBarButtonItemStyle. (.Plain by default)
+	- parameter closure: The closure to be called when the button is tapped
+	- returns: Newly initialized item with the specified properties.
+	*/
 	convenience init(image: UIImage, landscapeImagePhone: UIImage? = nil, style: UIBarButtonItemStyle = .Plain, closure: () -> Void) {
 		
 		self.init(image: image,
@@ -26,6 +38,14 @@ public extension UIBarButtonItem {
 		ActionKitSingleton.sharedInstance.addBarButtonItemClosure(self, closure: closure)
 	}
 	
+	/**
+	Initializes a new item using the specified title and other properties.
+	
+	- parameter title: The item’s title.
+	- parameter style: The style of the item. One of the constants defined in UIBarButtonItemStyle. (.Plain by default)
+	- parameter closure: The closure to be called when the button is tapped
+	- returns: Newly initialized item with the specified properties.
+	*/
 	convenience init(title: String, style: UIBarButtonItemStyle = .Plain, closure: () -> Void) {
 		self.init(title: title,
 		          style: style,
@@ -35,6 +55,13 @@ public extension UIBarButtonItem {
 		ActionKitSingleton.sharedInstance.addBarButtonItemClosure(self, closure: closure)
 	}
 	
+	/**
+	Initializes a new item containing the specified system item.
+	
+	- parameter systemItem: The system item to use as the first item on the bar. One of the constants defined in UIBarButtonSystemItem.
+	- parameter closure: The closure to be called when the button is tapped
+	- returns: Newly initialized item with the specified properties.
+	*/
 	convenience init(barButtonSystemItem systemItem: UIBarButtonSystemItem, closure: () -> Void) {
 		self.init(barButtonSystemItem: systemItem,
 		          target: ActionKitSingleton.sharedInstance,
@@ -43,6 +70,9 @@ public extension UIBarButtonItem {
 		ActionKitSingleton.sharedInstance.addBarButtonItemClosure(self, closure: closure)
 	}
 	
+	/** 
+	The closure to be called when the button is tapped.
+	*/
 	var closure: (() -> Void)? {
 		get {
 			return ActionKitSingleton.sharedInstance.barButtonItemClosure(self)
