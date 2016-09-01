bc18811c1015b0e5ebd316ff5e332138fe10573c
Mattijs Fuijkschot <mattijs@innovactory.com>
2016-09-01 12:47:12 +0200

    Fixed memory leaks by using associative references to hold closure refs instead of a dict
    Replaced the way self is provided to these "control" type closures. Which was originally holding a strong ref to self causing also, a memory leak.
    Removed the name argument from the gesture recognizer as there is no real use and doesn't work well together with the associative ref approach

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -30,7 +30,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let startDate = NSDate()
         //: ##Adding UIControl Events
         //:
         //: Old style of setting a target and action for the button
@@ -38,15 +38,22 @@ class ViewController: UIViewController {
                                 forControlEvents: .TouchUpInside)
 
         // This is equivalent to oldTestButton's implementation of setting the action to Tapped
-        testButton.addControlEvent(.TouchUpInside) {
-            self.testButton.setTitle("Tapped!", forState: .Normal)
+        testButton.addControlEvent(.TouchUpInside) { [weak self] in
+            self?.testButton.setTitle("Tapped! @ \(Int(NSDate().timeIntervalSinceDate(startDate)))sec", forState: .Normal)
+            self?.testButton.removeControlEvent(.TouchUpInside);
         }
 
         //: This adds a closure to the second button on the screen to change the text to Tapped2! when being tapped
-        testButton2.addControlEvent(.TouchUpInside, closure: {
-            self.testButton2.setTitle("Tapped2!", forState: .Normal)
-        })
+        testButton2.addControlEvent(.TouchUpInside, closure: { [weak self] in
+            self?.testButton2.setTitle("Tapped2!", forState: .Normal)
+            })
+        
+        // This adds a closure to the second button on the screen to change the text to Tapped2! when being tapped
+        testButton2.addControlEvent(.TouchUpInside, closure: { [weak self] in
+            self?.testButton2.setTitle("Tapped2!", forState: .Normal)
+            })
 
+        
         //: This adds a closure, which receives the UIControl as input parameter, to the third button.
         //: It shows how the UIControl can be mapped to the UIButton, in order to have its title changed.
         testButton3.addControlEvent(.TouchUpInside) { (button: UIButton) in
@@ -55,8 +62,8 @@ class ViewController: UIViewController {
 
         //: The following shows that you can remove a control event that has been set.
         //: Initially, tapping the first button on the screen would set the text to "Tapped!" ...
-        inactiveButton.addControlEvent(.TouchUpInside) {
-            self.inactiveButton.setTitle("Tapped!", forState: .Normal)
+        inactiveButton.addControlEvent(.TouchUpInside) { [weak self] in
+            self?.inactiveButton.setTitle("Tapped!", forState: .Normal)
         }
         
         //: ... but then the following removes that.
@@ -66,19 +73,19 @@ class ViewController: UIViewController {
         //: #Adding GestureRecognizers
         //:
         //: Add a Tap Gesture Recognizer (tgr)
-        let tgr = UITapGestureRecognizer(name: "setRed") {
+        let tgr = UITapGestureRecognizer() {
             self.view.backgroundColor = UIColor.redColor()
         }
         
         //: The following three lines will add an additional action to the red color gesture recognizer.
         //:  Multiple actions per gesture recognizer (or control events) are possible.
-        tgr.addClosure("setButton1") {
-            self.testButton.setTitle("Gesture: tapped once!", forState: .Normal)
+        tgr.addClosure() { [weak self] in
+            self?.testButton.setTitle("Gesture: tapped once!", forState: .Normal)
         }
         
         //: Add a Double Tap Gesture Recognizer (dtgr)
-        let dtgr = UITapGestureRecognizer(name: "setYellow") {
-            self.view.backgroundColor = UIColor.yellowColor()
+        let dtgr = UITapGestureRecognizer() { [weak self] in
+            self?.view.backgroundColor = UIColor.yellowColor()
         }
         dtgr.numberOfTapsRequired = 2
         
@@ -91,21 +98,13 @@ class ViewController: UIViewController {
         //: The following adds a long press gesture recognizer to the background view.
         //: It also shows it is not necessary to keep a reference to the gesture recognizer
         //: when you only need it inside the closure
-        view.addGestureRecognizer(UILongPressGestureRecognizer() { (gesture: UILongPressGestureRecognizer) in
+        view.addGestureRecognizer(UILongPressGestureRecognizer() { [weak self] (gesture: UILongPressGestureRecognizer) in
             if gesture.state == .Began {
-                let locInView = gesture.locationInView(self.view)
-                self.testButton2.setTitle("\(locInView)", forState: .Normal)
+                guard let strongSelf = self else { return }
+                let locInView = gesture.locationInView(strongSelf.view)
+                strongSelf.testButton2.setTitle("\(locInView)", forState: .Normal)
             }
         })
-		
-        // This adds a closure to the second button on the screen to change the text to Tapped2! when being tapped
-        testButton2.addControlEvent(.TouchUpInside, closure: {
-            self.testButton2.setTitle("Tapped2!", forState: .Normal)
-            })
-        
-        // This shows that you can remove a control event that has been set. Originally, tapping the first button on the screen
-        // would set the text to tapped! (line 31), but this removes that.
-        testButton.removeControlEvent(.TouchUpInside);
 		
 		// UIBarButton item
 		let titleItem = UIBarButtonItem(title: "Press me") { 
